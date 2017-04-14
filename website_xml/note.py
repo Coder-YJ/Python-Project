46cd68b0ca123fc63c493865f5f39a9277349fc8
GeniusYJ <1169571963@qq.com>
2017-04-14 23:27:32 +0800

    fix up project website_xml

---
+++ website_xml/note.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Build in 2017/4/14 by QianYuanJing
+'''
+    2017/4/14
+    project: website_xml
+    1、继承多个超类时需要注意顺序,前面超类中的函数会覆盖后面超类中相同的函数。
+    2、注意可变参数 *para 的应用
+    3、写程序要细心，否则小错误很难查找
+    
+'''
--- website_xml/website.py
+++ website_xml/website.py
@@ -7,21 +7,21 @@ from xml.sax import parse
 import os
 
 class Dispathcer:
-    def dispatch(self,prefix,name,attrs=None):
+    def dispatch(self, prefix, name, attrs=None):
         mname = prefix + name.capitalize()
         dname = 'default' + prefix.capitalize()
-        method = getattr(self,mname,None)
-#        args = ()
+        method = getattr(self, mname, None)
+        args = ()
         if callable(method):agrs = ()
         else:
-            method = getattr(self,dname,None)
+            method = getattr(self, dname, None)
             args = name,
         if prefix == 'start': args += attrs,
-        if callable(method):method(args)
-    def startElement(self,name,attrs):
-        self.dispatch('start',name,attrs)
-    def endElement(self,name,attrs):
-        self.dispatch('end',name)
+        if callable(method):method(*args)
+    def startElement(self, name, attrs):
+        self.dispatch('start', name, attrs)
+    def endElement(self, name):
+        self.dispatch('end', name)
                 
 class WebsiteConstructor(Dispathcer,ContentHandler):
     passthrough = False
@@ -33,42 +33,29 @@ class WebsiteConstructor(Dispathcer,ContentHandler):
         if not os.path.isdir(path):os.makedirs(path)
     def characters(self,chars):
         if self.passthrough:self.out.write(chars)
-    def defaultStart(self,name):
+    def defaultStart(self,name,attrs):
         if self.passthrough:
-            self.out.write('<' + name)
+            self.out.write('<' + name+ ' ') #name 后加空格以便将元素名 和 属性用空格隔开
             for key,val in attrs.items():
                 self.out.write('%s="%s"'%(key,val))
             self.out.write('>')
     def defaultEnd(self,name):
         if self.passthrough:
-            self.out.write('</%s>'%name)        
-    def startPage(self,attrs):
-        print('Beginning Page',attrs['name'])
-    def endPage(self):
-        print('Ending Page')
-    def defaultStart(self,name):
-        if self.passthrough:
-            self.out.write('<' + name)
-            for key,val in attrs.items():
-                self.out.write('%s="%s"'%(key,val))
-            self.out.write('>')
-    def defaultEnd(self,name):
-        if self.passthrough:
-            self.out.write('</%s>'%name)
+            self.out.write('</%s>'%name)    
+
     def startDirectory(self,attrs):
-        self.directory.append(attrs)
+        self.directory.append(attrs['name'])
         self.ensureDirectory()
-    def endDirectory(self,attrs):
+    def endDirectory(self):
         self.directory.pop()    
     def startPage(self,attrs):
-        print(attrs.items())
-        filename = os.path.join(*self.directory+[attrs['name']+'.html'])
+        filename = os.path.join(*self.directory + [attrs['name']+'.html'])
         self.out = open(filename,'w')
         self.writeHeader(attrs['title'])
         self.passthrough = True
     def endPage(self):
         self.passthrough = False
-        self.writFooter()
+        self.writeFooter()
         self.out.close()        
     def writeHeader(self,title):
         self.out.write('<html>\n <head>\n <title>')
