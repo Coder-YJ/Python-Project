52231461059f1743cf37ffd190da22fed9a959b7
GeniusYJ <1169571963@qq.com>
2017-05-09 19:37:57 +0800

    alter project webapp

--- webapp/test.py
+++ webapp/test.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # build in 2017/5/8 by QianYuanJing
-import orm
-from model import User,Blog,Comment
+import orm,asyncio
+from models import User, Blog, Comment
 
-def test():
-    yield from orm.create_pool(user='www-data',password='www-data',database='awesome')
-    
-    u = User(name='Test', email='test@example.com', passwd='1234567890',image='about:blank')
-    
-    yield from u.save()
-    
-for x in test():
-        pass
+async def test(loop):
+
+    await orm.create_pool(loop=loop,host='127.0.0.1',user='root', password='qianyuanjing', database='awesome')
+    u = User(name='qyj', email='qyj@example.com', passwd='0987654321', image='about:blank')
+    await u.save()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(test(loop))
+loop.close()
